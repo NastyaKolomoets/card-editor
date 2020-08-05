056b50ad1d9bc93ef4e4e37bc0af0df77081ed42
Anastasiia Kolomoiets <Anastasiia.Kolomoiets@ratesetter.com>
2020-08-05 19:05:41 +0300

    Create other door card.

---
+++ src/app/cards/munchkin/cards/doors/other-door/model/other-door-type.ts
@@ -0,0 +1,14 @@
+import { CardType, LayoutType } from 'src/app/cards/infrastructure/cards';
+
+export const OtherDoorType: CardType = {
+  name: 'otherDoor',
+  namePlural: 'otherDoors',
+  displayName: 'Інше',
+  displayNamePlural: 'Інше',
+  availableLayouts: [
+    LayoutType.IMAGE_TOP,
+    LayoutType.IMAGE_CENTER,
+    LayoutType.IMAGE_BOTTOM,
+    LayoutType.IMAGE_OVERLAP
+  ]
+};
