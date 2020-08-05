501b9c454f369dbe72bcae2c4902caa1a66db5d5
Anastasiia Kolomoiets <Anastasiia.Kolomoiets@ratesetter.com>
2020-08-05 12:15:42 +0300

    Create curse card.

---
+++ src/app/cards/munchkin/cards/doors/curse/curse.ts
@@ -0,0 +1,11 @@
+import { CurseType } from '../../../models/card-types/door-types';
+import { CurseCard } from './model/curse-card';
+import { CurseCardComponent } from './card/curse-card.component';
+import { CurseEditComponent } from './edit/curse-edit.component';
+
+export {
+  CurseType,
+  CurseCard,
+  CurseCardComponent,
+  CurseEditComponent
+};
