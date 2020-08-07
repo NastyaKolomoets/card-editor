4f53d43c6cea9a9a58d3875b8225edacfa2e2cad
Anastasiia Kolomoiets <Anastasiia.Kolomoiets@ratesetter.com>
2020-08-07 11:21:07 +0300

    Add treasures card group.

---
+++ src/app/cards/munchkin/cards/treasures/treasure-group.ts
@@ -0,0 +1,7 @@
+import { CardGroup } from 'src/app/cards/infrastructure/cards';
+
+export const Treasures: CardGroup = {
+  name: 'treasure',
+  displayName: 'Скарб',
+  displayNamePlural: 'Скарби'
+};
