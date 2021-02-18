62e1e6a2e0b375a40717101d27d7dc3ecd7f6699
Anastasiia Kolomoiets <Anastasiia.Kolomoiets@ratesetter.com>
2021-02-18 22:15:17 +0200

    Lix level-up plural.

--- src/app/cards/munchkin/cards/treasures/level-up/model/level-up-type.ts
+++ src/app/cards/munchkin/cards/treasures/level-up/model/level-up-type.ts
@@ -4,7 +4,7 @@ export const LevelUpType: CardType = {
   name: 'levelUp',
   namePlural: 'levelUps',
   displayName: 'Отримай рівень',
-  displayNamePlural: 'Отримай рівень',
+  displayNamePlural: 'Отримай рівні',
   availableLayouts: [
     LayoutType.LARGE_TEXT
   ]
