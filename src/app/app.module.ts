012a76c0adb88f00e9313da5e0a5e5cbd8783b6f
Yaroslav Kormushyn <ykormushyn@gmail.com>
2021-05-12 23:34:57 +0300

    clean up some imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import {AngularFirestore, AngularFirestoreModule} from '@angular/fire/firestore';
-import { SETTINGS as AUTH_SETTINGS } from '@angular/fire/auth';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavigationComponent } from './core/navigation/navigation.component';
-import { AngularFireModule } from '@angular/fire';
-import { environment } from '../environments/environment';
-import { AuthService } from './core/auth/auth.service';
-import { MunchkinModule } from './cards/munchkin/munchkin.module';
-import 'froala-editor/js/plugins.pkgd.min.js';
 import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+import { AppRoutingModule } from 'app/app-routing.module';
+import { AppComponent } from 'app/app.component';
+import { environment } from 'environments/environment';
+import { MunchkinModule } from 'app/cards/munchkin/munchkin.module';
+import { NavigationComponent } from 'app/core/components';
+import { AuthService } from 'app/core/services';
 
 @NgModule({
   declarations: [
