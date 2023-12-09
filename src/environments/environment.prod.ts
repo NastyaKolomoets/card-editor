a6914b793a9f779371da5d93d8d8188160ce963b
Anastasiia Kolomoiets <Anastasiia.Kolomoiets@ratesetter.com>
2023-12-09 19:54:05 +0200

    use another string replacement action for github pages workflow

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,10 +4,10 @@ export const environment = {
     // ***********************************************************************************************************************
     // * TODO(DEVELOPER): Update values according to: Firebase Console > Overview > Add Firebase to your web app. *
     // ***********************************************************************************************************************
-    apiKey: "",
+    apiKey: "$apiKey",
     authDomain: "localhost",
-    databaseURL: "https://munchkin-card-editor.firebaseio.com",
-    projectId: "munchkin-card-editor",
-    storageBucket: "gs://munchkin-card-editor.appspot.com"
+    databaseURL: "$databaseURL",
+    projectId: "$projectId",
+    storageBucket: "$storageBucket"
   }
 };
